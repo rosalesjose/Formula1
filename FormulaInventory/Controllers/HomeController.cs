cde488d491b449081178b2a9a9024d3e51d70761
Jose <jrosales0001@gmail.com>
2018-02-26 14:10:12 -0500

    add controllers

---
+++ FormulaInventory/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormulaInventory.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AddNewEngine()
+        { 
+            return View("AddEngineView");
+        }
+
+        public ActionResult AddNewWheel()
+        {
+            return View("AddWheelView");
+        }
+
+        public ActionResult AddToInventory()
+        {
+            return View("AddToInventory");
+        }
+    }
+}
